1705eb9d9b7508c68cc72feb97b2a9c1478f9aba
AnnaMut <butorovaanna@yandex.ru>
2018-09-26 23:57:02 +0300

    Разработка

--- js/game-screen.js
+++ js/game-screen.js
@@ -6,7 +6,6 @@ const ONE_SECOND = 1000;
 
 export default (state) => {
   const content = `<section class="game-page">
-${header(state)}
 <ul class="game-list">
   ${[...(initialState.scr)].map((i) => {
     return `<li><img class="close visually-hidden" src="img/closeCard.png" width="110" height="275" alt="Закрытая карта"><img class="open" src="${i}" width="110" height="275" alt="Карта"></li>`;
@@ -42,6 +41,12 @@ ${header(state)}
   startTimer();
 
   const gameScreen = getElementFromTemplate(content);
+  gameScreen.insertBefore(getElementFromTemplate(header(state)), gameScreen.firstChild);
+
+  const updateHeader = (somestate) => {
+    gameScreen.replaceChild(getElementFromTemplate(header(somestate)), gameScreen.firstChild);
+  };
+
 
   const replayButton = gameScreen.querySelector(`.replay-link`);
 
@@ -54,35 +59,42 @@ ${header(state)}
 
   const openCardClickHandler = (evt) => {
     stopTimer();
-    evt.target.classList.add(`visually-hidden`);
-    evt.target.nextElementSibling.classList.remove(`visually-hidden`);
-    evt.target.parentElement.classList.add(`choose`);
+    // evt.target.classList.add(`visually-hidden`);
+    // evt.target.nextElementSibling.classList.remove(`visually-hidden`);
+    // evt.target.parentElement.classList.add(`choose`);
+    evt.target.classList.toggle(`visually-hidden`);
+    evt.target.nextElementSibling.classList.toggle(`visually-hidden`);
+    evt.target.parentElement.classList.toggle(`choose`);
+
+    let choosenCards = gameScreen.querySelectorAll(`.choose img:not(.visually-hidden):not(.hidden)`);
+    let newScore;
+    if (choosenCards && choosenCards.length) {
+      if ((choosenCards[0].src === choosenCards[1].src)) {
+        newScore = state.score + (gameScreen.querySelectorAll(`.close`).length * 42);
+        choosenCards.forEach((item) => {
+          item.classList.add(`hidden`);
+        });
+      } else {
+        newScore = state.score - 84;
+        setTimeout(() =>{
+          choosenCards.forEach((item) => {
+            item.parentElement.classList.remove(`choose`);
+            item.classList.add(`visually-hidden`);
+            item.previousElementSibling.classList.remove(`visually-hidden`);
+          });
+        }, ONE_SECOND);
+      }
+    }
+    const newState = Object.assign({}, initialState, {score: newScore});
+    updateHeader(newState);
+    choosenCards.forEach((item) => {
+      item.removeEventListener(`click`, openCardClickHandler);
+    });
   };
 
   gameScreen.querySelectorAll(`.close`).forEach((item) => {
     item.addEventListener(`click`, openCardClickHandler);
   });
-
-  const checkCardsHandler = ()=> {
-    let ch = gameScreen.querySelectorAll(`.choose img:not(.visually-hidden):not(.hidden)`);
-    if (ch && ch.length) {
-      if ((ch[0].src === ch[1].src)) {
-        state.score += (gameScreen.querySelectorAll(`.close`).length * 42);
-        ch.forEach((item) => {
-          item.classList.add(`hidden`);
-        });
-      } else {
-        state.score -= 84;
-        ch.forEach((item) => {
-          item.parentElement.classList.remove(`choose`);
-          item.classList.add(`visually-hidden`);
-          item.previousElementSibling.classList.remove(`visually-hidden`);
-        });
-      }
-    }
-  };
-
-  gameScreen.addEventListener(`click`, checkCardsHandler);
 
   return gameScreen;
 };
