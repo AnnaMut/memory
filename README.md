a7dd92665c88d4acf65ad8f70ed059f97a361809
AnnaMut <butorovaanna@yandex.ru>
2018-10-11 15:48:33 +0300

    трэвис

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
+Игра на память
+
+[![Build Status](https://travis-ci.org/AnnaMut/memory.svg?branch=master)](https://travis-ci.org/AnnaMut/memory.svg?branch=master)
+
+
 Посмотреть можно :  https://annamut.github.io/memory/.
--- build/js/main.js
+++ build/js/main.js
@@ -14,11 +14,84 @@
     mainElement.appendChild(element);
   };
 
+  class AbstractView {
+    constructor() {
+      if (new.target === AbstractView) {
+        throw new Error(`Can't instantiate AbstractView, only concrete one`);
+      }
+    }
+
+    get template() {
+      throw new Error(`Template is required`);
+    }
+
+    get element() {
+      if (this._element) {
+        return this._element;
+      }
+      this._element = this.render();
+      this.bind(this._element);
+      return this._element;
+    }
+
+    render() {
+      return getElementFromTemplate(this.template);
+    }
+
+    bind() {
+    }
+
+  }
+
   var header = (state) => `<section class="game-page">
 <header class="game-header">
   <a class="replay-link" href="#">Начать заново</a>
   <p class="game-point">Очки: ${state.score}</p>
 </header>`;
+
+  class GameView extends AbstractView {
+    constructor(state) {
+      super();
+      this.state = state;
+    }
+
+    get template() {
+      return `<section class="game-page">
+  <ul class="game-list">
+    ${[...(this.state.scr)].map((i) => {
+    return `<li><img class="close visually-hidden" src="img/closeCard.png" width="110" height="275" alt="Закрытая карта"><img class="open" src="${i}" width="110" height="275" alt="Карта"></li>`;
+  }).join(``)}
+  </ul>
+  </section>`;
+    }
+
+    replayButtonClickHandler() {}
+
+    openCardClickHandler() {}
+
+    updateHeader() {}
+
+    bind() {
+      this.element.insertBefore(getElementFromTemplate(header(this.state)), this.element.firstChild);
+
+      const replayButton = this.element.querySelector(`.replay-link`);
+
+      replayButton.addEventListener(`click`, () => {
+        this.replayButtonClickHandler();
+      });
+
+      this.updateHeader = (somestate) => {
+        this.element.replaceChild(getElementFromTemplate(header(somestate)), this.element.firstChild);
+      };
+
+      this.element.querySelectorAll(`.close`).forEach((item) => {
+        item.addEventListener(`click`, (evt) => {
+          const chosenCard = evt.target;
+          this.openCardClickHandler(chosenCard);
+        });
+      });
+    }
+  }
 
   const CARDS_COUNT = 18;
 
@@ -54,15 +127,8 @@
   const ONE_SECOND = 1000;
 
   var gameScreen = (state) => {
-    const content = `<section class="game-page">
-${header(state)}
-<ul class="game-list">
-  ${[...(initialState.scr)].map((i) => {
-    return `<li><img class="close visually-hidden" src="img/closeCard.png" width="110" height="275" alt="Закрытая карта"><img class="open" src="${i}" width="110" height="275" alt="Карта"></li>`;
-  }
-  ).join(``)}
-</ul>
-</section>`;
+
+    const screen = new GameView(state);
 
     const tick = () => {
       state.time++;
@@ -73,10 +139,10 @@ ${header(state)}
     };
 
     const startTimer = () => {
-      const time = setTimeout(() => {
+      state.time = setTimeout(() => {
         tick();
         startTimer();
-        if (time > 10) {
+        if (state.time > 10) {
           stopTimer();
           document.querySelectorAll(`.close`).forEach((item) => {
             item.classList.remove(`visually-hidden`);
@@ -90,40 +156,78 @@ ${header(state)}
 
     startTimer();
 
-    const gameScreen = getElementFromTemplate(content);
-
-    const replayButton = gameScreen.querySelector(`.replay-link`);
-
-    const replayButtonClickHandler = ()=> {
+    screen.openCardClickHandler = (card) => {
       stopTimer();
-      changeScreen(welcomeScreen());
+      card.classList.toggle(`visually-hidden`);
+      card.nextElementSibling.classList.toggle(`visually-hidden`);
+      card.parentElement.classList.toggle(`choose`);
+
+      let choosenCards = document.querySelectorAll(`.choose img:not(.visually-hidden):not(.hidden)`);
+      let newScore;
+      if (choosenCards && choosenCards.length) {
+        if ((choosenCards[0].src === choosenCards[1].src)) {
+          newScore = document.querySelectorAll(`.close`).length * 42;
+          choosenCards.forEach((item) => {
+            item.classList.add(`hidden`);
+          });
+        } else {
+          newScore = -84;
+          setTimeout(() =>{
+            choosenCards.forEach((item) => {
+              item.parentElement.classList.remove(`choose`);
+              item.classList.add(`visually-hidden`);
+              item.previousElementSibling.classList.remove(`visually-hidden`);
+            });
+          }, ONE_SECOND);
+        }
+      }
+      const newState = Object.assign({}, initialState, {score: state.score + newScore});
+      screen.updateHeader(newState);
     };
 
-    replayButton.addEventListener(`click`, replayButtonClickHandler);
+    screen.replayButtonClickHandler = () => {
+      stopTimer();
+      changeScreen(welcomeScreen(initialState));
+    };
 
-    return gameScreen;
+    return screen.element;
   };
 
-  var welcomeScreen = () => {
-    const content = `<section class="start-page">
-<a class="start-img" href="#">
-  <img src="img/StartGame.png" width="503" height="262" alt="Раскладка карт">
-</a>
-<h2 class="start-title">MEMORY GAME</h2>
-<button class="start-button" type="button">Начать игру</button>  
-</section>`;
+  class WelcomeView extends AbstractView {
+    constructor() {
+      super();
+    }
 
-    const welcomeScreen = getElementFromTemplate(content);
+    get template() {
+      return `<section class="start-page">
+    <a class="start-img" href="#">
+      <img src="img/StartGame.png" width="503" height="262" alt="Раскладка карт">
+    </a>
+    <h2 class="start-title">MEMORY GAME</h2>
+    <button class="start-button" type="button">Начать игру</button>  
+    </section>`;
+    }
 
-    const startButton = welcomeScreen.querySelector(`.start-button`);
+    startButtonClickHandler() {}
 
-    const startButtonClickHandler = ()=> {
+    bind() {
+      const startButton = this.element.querySelector(`.start-button`);
+
+      startButton.addEventListener(`click`, () => {
+        this.startButtonClickHandler();
+      });
+
+    }
+  }
+
+  var welcomeScreen = (state) => {
+    const screen = new WelcomeView(state);
+
+    screen.startButtonClickHandler = () => {
       changeScreen(gameScreen(initialState));
     };
 
-    startButton.addEventListener(`click`, startButtonClickHandler);
-
-    return welcomeScreen;
+    return screen.element;
   };
 
   changeScreen(welcomeScreen());
